82a37f460632ac82126aab0a3fb72f5a123aee8f
b0c-beep <bocmarioadrian98@gmail.com>
2023-10-12 21:58:31 +0300

    figured barcode generator

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import qrcode as qr
 from PIL import Image
+from barcode import EAN13
+from barcode.writer import ImageWriter
 
-data = input("QR: ")
+choice = input("Cod de bare (1) sau QR (2)?")
 
-img = qr.make(data)
-img.save("qr.png");
+if choice == "2":
+    data = input("QR: ")
 
-im = Image.open("qr.png")
-im.show();
+    img = qr.make(data)
+    img.save("qr.png");
+
+    im = Image.open("qr.png")
+    im.show();
+
+
+elif choice == "1":
+    number = input("Numar (12 cifre): ")
+
+    code = EAN13(number, writer = ImageWriter())
+    code.save("barcode")
+
+    im = Image.open("barcode.png")
+    im.show()
